938269e53d92990b99a316d3448a687a8e9229a5
Phidung-hub <dungho01092002@gmail.com>
2023-10-23 16:05:59 +0800

    chore: forge init

---
+++ test/TestSignature.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract MockSignature is Test {
+  function testSignature() public {
+    assert(true);
+  }
+}
